4f03d83d41b6272fb1111c5f7940335c5b7de77a
Himanshu <himanshu.a.04.1999@gmail.com>
2025-02-25 10:17:44 -0500

    remove console logs

--- app/api/tasks/delete/[id]/route.js
+++ app/api/tasks/delete/[id]/route.js
@@ -2,7 +2,7 @@ import { deleteTask } from "@/actions/tasks";
 
 export async function DELETE(request, { params }) {
   const id = (await params).id;
-  console.log(id);
+  //   console.log(id);
   const task = await deleteTask(id);
   return Response.json({ message: "done", task: task });
 }
--- app/api/tasks/get/[id]/route.js
+++ app/api/tasks/get/[id]/route.js
@@ -2,7 +2,7 @@ import { getTask } from "@/actions/tasks";
 
 export async function GET(request, { params }) {
   const id = (await params).id;
-  console.log(id);
+  //   console.log(id);
   const task = await getTask(id);
   return Response.json({ message: "done", task: task });
 }
