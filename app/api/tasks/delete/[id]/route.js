a4ec62865a529bd51d653f0162a0709f141fd817
Himanshu <himanshu.a.04.1999@gmail.com>
2025-03-06 08:15:46 -0500

    Add Revalidate path to route handlers

--- app/api/tasks/delete/[id]/route.js
+++ app/api/tasks/delete/[id]/route.js
@@ -1,8 +1,10 @@
 import { deleteTask } from "@/actions/tasks";
+import { revalidatePath } from "next/cache";
 
 export async function DELETE(request, { params }) {
   const id = (await params).id;
   //   console.log(id);
   const task = await deleteTask(id);
+  revalidatePath("/");
   return Response.json({ message: "done", task: task });
 }
--- app/api/tasks/mark-done/[id]/route.js
+++ app/api/tasks/mark-done/[id]/route.js
@@ -1,8 +1,10 @@
 import { markTaskAsDone } from "@/actions/tasks";
+import { revalidatePath } from "next/cache";
 
 export async function POST(request, { params }) {
   const id = (await params).id;
   console.log(id);
   const task = await markTaskAsDone(id);
+  revalidatePath("/", "layout");
   return Response.json({ message: "done", task: task });
 }
