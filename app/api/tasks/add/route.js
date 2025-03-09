513d09af53489a891a1068b904ceb9a7059f10df
Himanshu <himanshu.a.04.1999@gmail.com>
2025-03-09 18:34:03 -0400

    Clean code a bit
    removed lines revalidating home page.
    the no-cache on home page does the job it seems

--- app/api/tasks/add/route.js
+++ app/api/tasks/add/route.js
@@ -3,9 +3,6 @@ import { revalidatePath } from "next/cache";
 
 export async function POST(request) {
   const body = await request.json();
-  //   console.log(body);
   const saved = await addTask(body.name, body.description);
-  // console.log(saved);
-  revalidatePath("/");
   return Response.json({ message: "done", saved: saved });
 }
--- app/api/tasks/mark-done/[id]/route.js
+++ app/api/tasks/mark-done/[id]/route.js
@@ -5,6 +5,5 @@ export async function POST(request, { params }) {
   const id = (await params).id;
   console.log(id);
   const task = await markTaskAsDone(id);
-  revalidatePath("/");
   return Response.json({ message: "done", task: task });
 }
